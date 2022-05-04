088f9a956d3265865af71661b4c808ef3e2abb2b
HenriqueTardoqui <henrique.tardoqui@gmail.com>
2022-05-04 14:57:42 -0300

    almost done

--- routers/artistas.routes.js
+++ routers/artistas.routes.js
@@ -3,30 +3,67 @@ const router = require("express").Router();
 const ArtistasModel = require("../models/Artista.model");
 
 router.post("/add-artistas", async (req, res) => {
+	try {
+		const artistaObj = req.body;
+
+		const createdObj = await ArtistasModel.create(artistaObj);
+
+		return res.status(201).json(createdObj);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json(error);
+	}
+});
+
+
+
+
+router.get("/:artistaId", async (req, res) => {
+	try {
+		const { artistaId } = req.params;
+
+		const foundedAstist = await ArtistasModel.findOne({ _id: artistaId });
+
+		return res.status(200).json(foundedAstist);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.patch("/update-artista/:artistaId", async (req, res) => {
+	try {
+		const { artistaId } = req.params;
+        
+		const updatedObj = await ArtistasModel.findOneAndUpdate(
+			{ _id: artistaId },  
+			{ ...req.body },
+			{ runValidators: true, new: true },
+
+		);
+        return res.status(200).json(updatedObj);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json(error);
+	}
+});
+
+router.delete("/delete-artist/:artistaId", async (req, res) => {
     try{
 
-        const artistaObj = req.body;
+        const {artistaId} = req.params;
 
-        const createdObj = await ArtistasModel.create(artistaObj);
+        const deletedArtista = await ArtistasModel.deleteOne(
+            {_id: artistaId}
+        );
 
+            return res.status(200).json(deletedArtista);
 
-        return res.status(201).json(createdObj)
-    }
-    
-    catch(error){
+    } catch(error){
         console.error(error);
-        return res.status(500).json(error);
+		return res.status(500).json(error);
     }
-})  
-
-
-
-
-
-
+})
 
 
 
 module.exports = router;
-
-
