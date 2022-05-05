4cc7613df20fe232d6fc48d8b3d4ecdf3ac928c3
HenriqueTardoqui <henrique.tardoqui@gmail.com>
2022-05-05 20:18:07 -0300

    album almost done

--- routers/album.routes.js
+++ routers/album.routes.js
@@ -6,14 +6,16 @@ const ArtistaModel = require("../models/Artista.model")
 router.post("/add-album/:artistaId", async (req, res) => {
     try {
 
-        const createdAlbum = await AlbumModel.create (
-            {...req.body},
-            
-        );
+        const createdAlbum = await AlbumModel.create ({
+            ...req.body,
+            musics: req.body.artistaId,
+            main_artist: req.body.artistaId,
+            collaborators: req.body.artistaId
+        });
 
         const foundedArtist = await ArtistaModel.findOneAndUpdate(
             { _id: req.params.artistaId },
-            { $push: { albuns: createdAlbum._id } },
+            { $push: { albuns: createdAlbum } },
             { runValidators: true, new: true }
           );
         return res.status(200).json(createdAlbum)
@@ -23,5 +25,22 @@ router.post("/add-album/:artistaId", async (req, res) => {
 		return res.status(500).json(err);
 	}
 })
+
+router.get("/:albumId", async (req, res) => {
+    try {
+		const { albumId } = req.params;
+
+		const foundedAlbum = await AlbumModel.findOne({ _id: albumId });
+
+		return res.status(200).json(foundedAlbum);
+	} catch (error) {
+		console.log(error);
+         
+	}
+})
+
+
+
+
 
 module.exports = router;
